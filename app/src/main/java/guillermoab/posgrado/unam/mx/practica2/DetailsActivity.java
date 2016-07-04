1760732bdbf4cb47354c420d79cf27ce0a37f949
UDS_Estructuracion <guillermoab@posgrado.unam.mx>
2016-07-04 11:26:17 -0500

    Actionbar Activity Details finished

--- app/src/main/java/guillermoab/posgrado/unam/mx/practica2/DetailsActivity.java
+++ app/src/main/java/guillermoab/posgrado/unam/mx/practica2/DetailsActivity.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -20,8 +23,9 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     private TextView txtappname;
     private TextView txtdevname;
     private TextView txtdetails;
-    private int id,install;
+    private int id,img_int,install;
     private TextView chkInstall;
+    private static final int REQUEST_CODE_EDIT_APP = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,11 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         findViewById(R.id.details_btnUninstall).setOnClickListener(this);
         findViewById(R.id.details_btnOpen).setOnClickListener(this);
         findViewById(R.id.details_btnUpdate).setOnClickListener(this);
+        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.menu_details_title));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
         img = (ImageView) findViewById(R.id.details_imgAPP);
         txtappname=(TextView) findViewById(R.id.details_txtAppName);
         txtdevname=(TextView) findViewById(R.id.details_txtDevName);
@@ -40,7 +49,8 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
             txtappname.setText(getIntent().getExtras().getString("appName"));
             txtdevname.setText(getIntent().getExtras().getString("appDev"));
             txtdetails.setText(getIntent().getExtras().getString("appDetails"));
-            img.setImageResource(getIntent().getExtras().getInt("appImg"));
+            img_int=getIntent().getExtras().getInt("appImg");
+            img.setImageResource(img_int);
             install = getIntent().getExtras().getInt("appInstall");
             if(install==0){
                 chkInstall.setText(getResources().getString(R.string.msj_updated));
@@ -48,6 +58,35 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
                 chkInstall.setText(getResources().getString(R.string.msj_install));
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activitydetails,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.menu_details_edit:
+                Intent intent = new Intent(getApplicationContext(),EditActivity.class);
+                intent.putExtra("id",id);
+                String appName = txtappname.getText().toString();
+                String appDev = txtdevname.getText().toString();
+                String appDetails = txtdetails.getText().toString();
+                intent.putExtra("appName",appName);
+                intent.putExtra("appDev",appDev);
+                intent.putExtra("appDetails",appDetails);
+                intent.putExtra("appImg",img_int);
+                intent.putExtra("appInstall",install);
+                startActivity(intent);//,REQUEST_CODE_EDIT_APP);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -77,5 +116,10 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         String url = getResources().getString(R.string.details_url1);
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/guillermoab/posgrado/unam/mx/practica2/EditActivity.java
@@ -0,0 +1,16 @@
+package guillermoab.posgrado.unam.mx.practica2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by UDS_Estructuracion on 04/07/2016.
+ */
+public class EditActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
